4bc59676440a65321b6376b23bfd854395064f7d
yoramfeld <111964974+yoramfeld@users.noreply.github.com>
2024-06-13 13:09:59 +0300

    Added comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import requests
+import requests #for communicating with openweathermap site
 from datetime import datetime, timedelta
-options = []  #multile locations options for a city of the same name
+options = []  #multiple location options for a popular city name in several countries
 def main():
     st.title("World Cities Weather Information")
-    api_key = '7f0804cb4aab571b1c8921f4d82e1815'  # api key for open weather map site
-    city_name=st.text_input("Enter city name:")
-    if st.button("Go") or 'go' in st.session_state:  # e.g. 'Birmingham' #'Saint Petersburg' #'Jerusalem' #London
+    api_key = '7f0804cb4aab571b1c8921f4d82e1815'  # api key to access openweathermap site
+    city_name=st.text_input("Enter city name:") #get city name from user e.g. 'Jerusalem' 'London' 'Ramat Gan'
+    if st.button("Go") or 'go' in st.session_state:
         st.session_state['go'] = 'clicked'
         location = requests.get(
             'http://api.openweathermap.org/geo/1.0/direct?q=' + city_name + '&limit=50&appid=' + api_key)
@@ -34,9 +34,10 @@ def main():
             weather = w.json()['weather'][0]
             st.write(f"has {main_resp['temp']} degC and {main_resp['humidity']}% humidity with {weather['description']}")
             seconds_from_utc = w.json()['timezone']  # the remote location seconds from UTC, received from the API response
-            utcnow = datetime.utcnow()
+            utcnow = datetime.utcnow() #time at UTC
             remote_time = utcnow + timedelta(seconds=seconds_from_utc)
             st.write('The local time over there is', remote_time.strftime("%H:%M"), "on", remote_time.strftime("%d/%m"), end=' ')
             st.write("while this app server is at", datetime.now().strftime("%H:%M"), "on", datetime.now().strftime("%d/%m"))  # print the local time and date
+
 if __name__ == "__main__": #required for Streamlit
     main()
